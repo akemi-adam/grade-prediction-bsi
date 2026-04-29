26998d2a75f5a2124a5f1fdf66f9f035db573ca6
Mosiah Adam <tech.adam.servicos@gmail.com>
2026-04-29 12:00:02 -0300

    adiciona notas de todas as unidades para cada aluno mantendo a unicidade

--- test.py
+++ test.py
@@ -2,14 +2,12 @@ from pandas import DataFrame, read_csv, merge
 from enum import Enum
 
 # DF de componentes
-
 componentes: DataFrame = read_csv('componentes-curriculares-presenciais.csv', sep=';')
 componentes: DataFrame = componentes[['id_componente', 'tipo_componente', 'codigo', 'nome', 'unidade_responsavel', 'curso_componente']]
 
 # Somente alguns componentes com o código BSI estão com a informaçaõ do curso
 # Aqui eu procuro os componentes com código DCT/BSI e atualizo essa informação
 componentes['codigo'] = componentes['codigo'].astype(str)
-componentes.loc[componentes['codigo'].str.startswith(('DCT', 'BSI')), 'curso_componente'] = 'SISTEMAS DE INFORMAÇÃO'
 
 # DF de turmas
 turmas: DataFrame = read_csv('turmas-2017.1.csv', sep=';')
@@ -19,14 +17,11 @@ matriculas: DataFrame = read_csv('matricula-componente-20171.csv', sep=';')
 
 
 # Faz merge/join entre data frames
-
 df_temp: DataFrame = merge(componentes, turmas, left_on='id_componente', right_on='id_componente_curricular')
 df_temp = merge(df_temp, matriculas, on='id_turma')
 
 df_filtrado: DataFrame = df_temp[(
-    (df_temp['curso_componente'].notna()) &
-    (df_temp['tipo_componente'].notna()) &
-    (df_temp['curso_componente'] == 'SISTEMAS DE INFORMAÇÃO') &
+    (df_temp['codigo'].str.startswith(('DCT', 'BSI'))) &
     (df_temp['tipo_componente'] == 'DISCIPLINA')
 )]
 
@@ -54,11 +49,46 @@ class StatusMatricula(Enum):
         ]
 
 
-# Filtra por status válidos e elimina duplicatas de aluno na mesma turma
+# Filtra por status válidos
 df_filtrado = df_filtrado[df_filtrado['descricao'].isin(StatusMatricula.status_validos())]
+
+df_filtrado['unidade'] = df_filtrado['unidade'].astype(int)
+
+# Cria colunas de notas por unidade
+notas_pivot = df_filtrado.pivot_table(
+    index=['discente', 'id_turma'],
+    columns='unidade',
+    values='nota',
+    aggfunc='first'
+).rename(columns={
+    1: 'nota_unidade_1',
+    2: 'nota_unidade_2',
+    3: 'nota_unidade_3'
+})
+
+df_filtrado = df_filtrado.merge(
+    notas_pivot,
+    on=['discente', 'id_turma'],
+    how='left'
+)
+
 df_filtrado = df_filtrado.drop_duplicates(subset=['discente', 'id_turma'])
 
-df_final: DataFrame = df_filtrado[['id_componente', 'nome', 'id_turma', 'ano', 'periodo', 'discente', 'media_final', 'descricao']]
+df_final = df_filtrado[
+    [
+        'id_componente',
+        'nome',
+        'id_turma',
+        'ano',
+        'periodo',
+        'discente',
+        'nota_unidade_1',
+        'nota_unidade_2',
+        'nota_unidade_3',
+        'media_final',
+        'descricao'
+    ]
+]
 
-print(df_filtrado.head())
+print(df_final.shape)
 df_final.to_csv('final.csv')
